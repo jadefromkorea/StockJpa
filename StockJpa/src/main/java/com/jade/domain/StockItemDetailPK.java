ddd11fd3821fb1b9e20b328108aa38d7ba0f704d
jade <jadefromkorea@gmail.com>
2019-07-20 16:29:44 +0900

    commit

--- StockJpa/src/main/java/com/jade/domain/StockItemDetailPK.java
+++ StockJpa/src/main/java/com/jade/domain/StockItemDetailPK.java
@@ -3,6 +3,23 @@ package com.jade.domain;
 import java.io.Serializable;
 
 public class StockItemDetailPK implements Serializable{
+	private static final long serialVersionUID = 1L;
+	
 	private String stockNo;			// 종목코드
 	private String stockDate;		// 날짜
+	
+	public String getStockNo() {
+		return stockNo;
+	}
+	public void setStockNo(String stockNo) {
+		this.stockNo = stockNo;
+	}
+	public String getStockDate() {
+		return stockDate;
+	}
+	public void setStockDate(String stockDate) {
+		this.stockDate = stockDate;
+	}
+	
+	
 }
